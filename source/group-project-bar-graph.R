ddbfed041c51b8e306c61f31d1b2d05cb8d1f290
ChristinaCode <christina._.nguyen@outlook.com>
2022-11-14 21:50:58 -0800

    finished bar graph

--- source/group-project-bar-graph.R
+++ source/group-project-bar-graph.R
@@ -6,26 +6,27 @@ heart_disease <- read.csv("https://data.cdc.gov/api/views/uc9k-vc2j/rows.csv", h
 View(heart_disease)
 
 #I'm not sure if there's a certain order you want these filtered. - Luna
+#code for filtering
 heart_disease_filtered <- heart_disease %>% 
+  filter(Year == "2019") %>%
   filter(Stratification2 != "Overall") %>% 
   filter(LocationAbbr == "WA") %>%
-  drop_na(Data_value) %>%
-  filter(Data_Value_Unit != "%")
+  drop_na(Data_Value) %>%
+  filter(Data_Value_Unit != "%") %>%
+  select(LocationID,
+        Year,
+        LocationAbbr,
+        LocationDesc,
+        Data_Value,
+        Data_Value_Type,
+        Stratification1,
+        Stratification2,
+        Stratification3)
 
 View(heart_disease_filtered)
-
-select(LocationID,
-       Year,
-       LocationAbbr,
-       LocationDesc,
-       Data_Value,
-       Data_Value_Type,
-       Stratification1,
-       Stratification2,
-       Stratification3)
-
-ggplot(data = heart_disease_filter) +
-  geom_col(mapping = aes(x = Stratification2, fill = Stratification2))
+#code for making bar graph
+ggplot(data = heart_disease_filtered) +
+  geom_col(mapping = aes(x = Stratification2, y = Data_Value, fill = Stratification2))
 
 #take out overalls from race
 #take out all ages except 65+
