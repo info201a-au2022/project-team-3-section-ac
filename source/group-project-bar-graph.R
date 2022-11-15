53d34820243894e29ccc77b13a4603020e617721
ChristinaCode <christina._.nguyen@outlook.com>
2022-11-14 22:40:45 -0800

    fixed bar graph

--- source/group-project-bar-graph.R
+++ source/group-project-bar-graph.R
@@ -8,12 +8,12 @@ View(heart_disease)
 #I'm not sure if there's a certain order you want these filtered. - Luna
 #code for filtering
 heart_disease_filtered <- heart_disease %>% 
+  drop_na(Data_Value) %>%
   filter(Year == "2019") %>%
-  filter(Stratification1 == "Ages 65+ years")
+  filter(Stratification1 == "Ages 65+ years") %>%
   filter(Stratification2 != "Overall") %>%
   filter(Stratification3 == "Overall") %>%
   filter(LocationAbbr == "WA") %>%
-  drop_na(Data_Value) %>%
   filter(Data_Value_Unit != "%") %>%
   select(LocationID,
         Year,
@@ -23,12 +23,15 @@ heart_disease_filtered <- heart_disease %>%
         Data_Value_Type,
         Stratification1,
         Stratification2,
-        Stratification3)
+        Stratification3) %>%
+  rename(Race = Stratification2) %>%
+  rename(death_rate_per_100000 = Data_Value)
 
 View(heart_disease_filtered)
 #code for making bar graph
 ggplot(data = heart_disease_filtered) +
-  geom_col(mapping = aes(x = Stratification2, y = Data_Value, fill = Stratification2))
+  geom_col(mapping = aes(x = Race, y = death_rate_per_100000, fill = Race))
+  
 
 #take out overalls from race
 #take out all ages except 65+
