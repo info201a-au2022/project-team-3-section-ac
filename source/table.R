90aaf7d1693f6f9a434f34d18b1d44a682b8ecbd
Daria Manguling <darialynmanguling@Darias-MacBook-Air.local>
2022-11-14 20:14:49 -0800

    complete graph and table

--- source/table.R
+++ source/table.R
@@ -3,33 +3,36 @@ library(dplyr)
 
 
 filename <- "https://data.cdc.gov/api/views/uc9k-vc2j/rows.csv"
-heart_disease_chart <- read.csv(filename, header = TRUE, stringsAsFactors = FALSE)
+heart_disease_chart <- read.csv(filename, header = TRUE,
+                                stringsAsFactors = FALSE)
 View(heart_disease_chart)
-wa <- heart_disease_chart %>% 
-  drop_na(Data_Value) %>% 
-  filter(LocationAbbr == "WA") %>% 
-  select(LocationID,
-         Year,
-         LocationAbbr,
-         LocationDesc,
-         Data_Value,
-         Data_Value_Type,
-         Data_Value_Unit,
-         Stratification1,
-         Stratification2, 
-         Stratification3) %>%
-  rename(fips = LocationID,
-         State = LocationAbbr,
-         County = LocationDesc,
-         Age = Stratification1,
-         Race = Stratification2,
-         Sex = Stratification3,
-         Total_Percent_Change = Data_Value)
+wa <- heart_disease_chart %>%
+  drop_na(Data_Value) %>%
+  filter(LocationAbbr == "WA") %>%
+  select(
+    LocationID,
+    Year,
+    LocationAbbr,
+    LocationDesc,
+    Data_Value,
+    Data_Value_Type,
+    Data_Value_Unit,
+    Stratification1,
+    Stratification2,
+    Stratification3
+  ) %>%
+  rename(
+    fips = LocationID,
+    State = LocationAbbr,
+    County = LocationDesc,
+    Age = Stratification1,
+    Race = Stratification2,
+    Sex = Stratification3,
+    Total_Percent_Change = Data_Value
+  )
 
-wa_table <- wa %>% 
+wa_table <- wa %>%
   filter(Data_Value_Unit == "%" & Race == "Overall" & Sex == "Overall" &
-         Age == "Ages 65+ years") %>% 
-  group_by(County) %>% 
-  summarize(avg_stroke_rate_change = mean(Total_Percent_Change))
-
-View(wa_table)
+    Age == "Ages 65+ years") %>%
+  group_by(County) %>%
+  summarize(avg_stroke_rate_change = round(mean(Total_Percent_Change)))
