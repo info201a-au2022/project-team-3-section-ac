792dd6b7e4375c632fba6bc1e806258201e11957
Daria Manguling <darialynmanguling@Darias-MacBook-Air.local>
2022-12-06 22:36:43 -0800

    map caption added

--- shinyappwebsite/ui.R
+++ shinyappwebsite/ui.R
@@ -12,8 +12,7 @@ intro <- tabPanel(
 # input map choices
 map_input <- sidebarPanel(uiOutput("selectState"),
                           uiOutput("selectYear"),
-                          uiOutput("selectAge"),
-                          print("test"))
+                          uiOutput("selectAge"))
 
 # state map plot
 state_map <- mainPanel(plotlyOutput("map"))
@@ -25,7 +24,27 @@ map_page <- tabPanel(
   sidebarLayout(
     map_input,
     state_map
-  )
+  ),
+  p("The different state maps and their respective
+                            counties show the different cardiovascular disease
+                            and stroke death rates for either people ages 35-64
+                            years or people ages 65+ in each county of a given
+                            state and a given year. It shows the overall
+                            population rate rather than individual races or
+                            gender to paint a full picture of the entire
+                            population. The purpose of the map is to visualize 
+                            the counties that have the highest and lowest
+                            rates, and if there are any changes to the lowest
+                            and highest rates throughout the years. Looking at
+                            different county rates is important because one can
+                            analyze specific counties as to what feature
+                            differences they have that could influence the
+                            heart disease and stroke death rates that they
+                            have. One thing that has seemed to be consistent
+                            throughout the age populations is that rates for 
+                            35-64 years are much lower than 65+, which reflects 
+                            the fact that older people have a higher chance of
+                            dying of heart disease.")
 )
 
 # scatterplot panel
